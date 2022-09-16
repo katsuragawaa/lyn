c785d3007a41c86880a770ef4a67b54f88b562e5
katsuragawa <andre.katsuragawa@gmail.com>
2022-09-15 23:16:14 -0300

    add pix image

--- src/components/TattooSuggestion.tsx
+++ src/components/TattooSuggestion.tsx
@@ -1,5 +1,8 @@
+import pix from '../assets/pix.jpg';
+
 export const TattooSuggestion = () => (
-  <>
-    <div className="mx-auto mt-4 flex h-48 w-48 items-center justify-center bg-white">Pix</div>
-  </>
+  <div className="flex w-48 flex-col self-center">
+    <img className="mx-auto mt-4 flex h-48 items-center justify-center bg-white" src={pix} />
+    <div className="mt-4 text-center font-bold">Apoie essa causa</div>
+  </div>
 );
